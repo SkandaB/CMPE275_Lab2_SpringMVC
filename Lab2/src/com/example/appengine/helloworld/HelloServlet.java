42a7ea204e5380ddad81dfc158605b74ac83a7c7
Skanda Narendra Bhargav <skandabhargav@gmail.com>
2016-11-09 11:14:16 -0800

    Google Cloud Setup and Deploy from IDE

---
+++ Lab2/src/com/example/appengine/helloworld/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.example.appengine.helloworld;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// [START example]
+@SuppressWarnings("serial")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Hello, world");
+    }
+}
+// [END example]
