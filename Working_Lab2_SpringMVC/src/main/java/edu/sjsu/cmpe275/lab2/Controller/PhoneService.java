a88be2e884f943fd46b0ee429a12b86a1b675465
Skanda Narendra Bhargav <skandabhargav@gmail.com>
2016-11-18 21:46:46 -0800

    please get rid of this

--- Working_Lab2_SpringMVC/src/main/java/edu/sjsu/cmpe275/lab2/Controller/PhoneService.java
+++ Working_Lab2_SpringMVC/src/main/java/edu/sjsu/cmpe275/lab2/Controller/PhoneService.java
@@ -1,8 +1,42 @@
-package edu.sjsu.cmpe275.lab2.Controller;
+/**
+ * 
+ */
+package edu.sjsu.cmpe275.lab2.controller;
 
 import org.springframework.stereotype.Service;
 
+import edu.sjsu.cmpe275.lab2.entity.AddressEntity;
+import edu.sjsu.cmpe275.lab2.entity.PhoneEntity;
+
+/**
+ * @author SkandaBhargav
+ *
+ */
+
 @Service
 public class PhoneService {
+	public String PhoneCreation() {
+		
+		PhoneEntity phone=new PhoneEntity();
+		phone.setNumber("12345689");
+		AddressEntity phoneAddress=new AddressEntity();
+		phoneAddress.setCity("Latur");
+		phoneAddress.setStreet("mg nagar");
+		phoneAddress.setZip("40068");
+		phone.setAddress(phoneAddress);
+
+
+
+		PhoneEntity phone1=new PhoneEntity();
+		phone1.setNumber("0000000000");
+		AddressEntity phoneAddress1=new AddressEntity();
+		phoneAddress1.setCity("YYYY");
+		phoneAddress1.setStreet("XXXX");
+		phoneAddress1.setZip("9999");
+		phone1.setAddress(phoneAddress1);
+		
+		
+		return "welcome";
+	}
 
 }
